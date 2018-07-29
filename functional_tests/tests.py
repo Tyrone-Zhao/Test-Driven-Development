48f6e64096c63d45552897a0759ad9161384695f
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-07-29 20:47:43 +0800

    添加新的功能测试

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,11 @@
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import WebDriverException
 from selenium import webdriver
 from django.test import LiveServerTestCase
 import time
+
+
+MAX_WAIT = 10
 
 
 class NewVisitorTest(LiveServerTestCase):
@@ -13,13 +17,21 @@ class NewVisitorTest(LiveServerTestCase):
         time.sleep(10)
         self.browser.quit()
 
-    def check_for_row_in_list_table(self, row_text):
-        ''' 辅助方法 '''
-        table = self.browser.find_element_by_id("id_list_table")
-        rows = table.find_elements_by_tag_name("tr")
-        self.assertIn(row_text, [row.text for row in rows])
+    def wait_for_row_in_list_table(self, row_text):
+        ''' 重构后的辅助方法 '''
+        start_time = time.time()
+        while True:
+            try:
+                table = self.browser.find_element_by_id("id_list_table")
+                rows = table.find_elements_by_tag_name("tr")
+                self.assertIn(row_text, [row.text for row in rows])
+                return
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
 
-    def test_can_start_a_list_and_retrieve_it_later(self):
+    def test_can_start_a_list_for_one_user(self):
         # 小明听说有一个很酷的待办事项在线应用，于是去看了应用首页
         self.browser.get(self.live_server_url)
 
@@ -28,40 +40,74 @@ class NewVisitorTest(LiveServerTestCase):
             "Browser title was " + self.browser.title
 
         # 应用邀请他输入一个待办事项
-        inputbox = self.browser.find_elements_by_id("id_new_item")
+        inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertEqual(
-            inputbox[0].get_attribute("placeholder"),
+            inputbox.get_attribute("placeholder"),
             "输入一个待办事项"
         )
 
         # 他在一个文本框中输入了"购买孔雀羽毛"
         # 它的爱好是购买稀有的东西收藏起来
-        inputbox[0].send_keys("购买孔雀羽毛")
+        inputbox.send_keys("购买孔雀羽毛")
 
         # 他按回车键后，页面更新了
         # 待办事项表格中显示了"1: 购买孔雀羽毛"
-        inputbox[0].send_keys(Keys.ENTER)
-        time.sleep(1)
+        inputbox.send_keys(Keys.ENTER)
 
-        self.check_for_row_in_list_table("1: 购买孔雀羽毛")
+        self.wait_for_row_in_list_table("1: 购买孔雀羽毛")
 
         # 页面中又显示了一个文本框，可以输入其他的待办事项
         # 他输入了“把孔雀羽毛收藏到家里”
         # 他做事很有条理
-        inputbox[0] = self.browser.find_element_by_id("id_new_item")
-        inputbox[0].send_keys("把孔雀羽毛收藏到家里")
-        inputbox[0].send_keys(Keys.ENTER)
-        time.sleep(1)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("把孔雀羽毛收藏到家里")
+        inputbox.send_keys(Keys.ENTER)
 
         # 页面再次更新，他的清单中显示了这两个待办事项
-        self.check_for_row_in_list_table("1: 购买孔雀羽毛")
-        self.check_for_row_in_list_table("2: 把孔雀羽毛收藏到家里")
+        self.wait_for_row_in_list_table("1: 购买孔雀羽毛")
+        self.wait_for_row_in_list_table("2: 把孔雀羽毛收藏到家里")
 
-        # 小明想知道这个网站会否记住他的清单
-        # 他看到网站为他生成了一个唯一的URL
-        # 而且页面中有一些文字解说这个功能
-        self.fail("Finish the test")
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # 小明新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("购买孔雀羽毛")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 购买孔雀羽毛")
 
-        # 他访问那个URL，发现他的待办事项列表还在
+        # 他注意到清单有个唯一的URL
+        ming_list_url = self.browser.current_url
+        self.assertRegex(ming_list_url, "/lists/.+")
 
-        # 他很满意，去睡觉了
+        # 现在一名叫做小花的新用户访问了网站
+
+        # 我们使用一个新浏览器会话
+        # 确保小明的信息不回从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+
+        # 小花访问首页
+        # 页面中看不到小明的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("购买孔雀羽毛", page_text)
+        self.assertNotIn("把孔雀羽毛收藏到家里", page_text)
+
+        # 小花输入一个新的待办事项，新建一个清单
+        # 他不想小明一样兴趣盎然
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        inputbox.send_keys("买牛奶")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 买牛奶")
+
+        # 小花获得了他的唯一URL
+        hua_list_url = self.browser.current_url
+        self.assertRegex(hua_list_url, "/lists/.+")
+        self.assertNotEqual(hua_list_url, ming_list_url)
+
+        # 这个页面还是没有小明的清单
+        page_text = self.browser.find_element_by_tag_name("body").text
+        self.assertNotIn("购买孔雀羽毛", page_text)
+        self.assertIn("买牛奶", page_text)
+
+        # 两人都很满意，然后去睡觉了
