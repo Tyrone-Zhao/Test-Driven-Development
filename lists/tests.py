607b11026dfc091772af5748f043a18bdf437caf
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-07-29 22:07:54 +0800

    小花没有得到自己的清单页面，依然可以看到小明的待办事项

--- lists/tests.py
+++ lists/tests.py
@@ -21,31 +21,19 @@ class HomePageTest(TestCase):
         ''' 判断处理完POST请求后可以正确的重定向 '''
         response = self.client.post("/", data={"item_text": "一个新的待办事项"})
         self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"], "/")
+        self.assertEqual(response["location"],
+                         "/lists/the-only-list-in-the-world/")
 
     def test_only_saves_items_when_necessary(self):
         ''' 判断请求只保存需要的待办事项 '''
         self.client.get("/")
         self.assertEqual(Item.objects.count(), 0)
 
-    def test_display_all_list_items(self):
-        ''' 
-            测试页面table可以显示多个待办事项, 
-            空行表明:设置-使用-断言的单元测试典型结构 
-        '''
-        Item.objects.create(text="事项1")
-        Item.objects.create(text="事项2")
-
-        response = self.client.get("/")
-
-        self.assertIn("事项1", response.content.decode())
-        self.assertIn("事项2", response.content.decode())
-
 
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
-        ''' 测试数据库保存和获取待办事项 '''
+        ''' 测试数据库可以保存和获取待办事项 '''
         first_item = Item()
         first_item.text = "第一个列表项"
         first_item.save()
@@ -60,3 +48,21 @@ class ItemModelTest(TestCase):
         second_saved_item = saved_items[1]
         self.assertEqual(first_saved_item.text, "第一个列表项")
         self.assertEqual(second_saved_item.text, "第二个列表项")
+
+
+class ListViewTest(TestCase):
+
+    def test_uses_list_template(self):
+        ''' 测试清单视图使用了和首页不同的模版 '''
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+        self.assertTemplateUsed(response, "list.html")
+
+    def test_display_all_items(self):
+        ''' 测试应用首页可以显示创建过的待办事项 '''
+        Item.objects.create(text="事项1")
+        Item.objects.create(text="事项2")
+
+        response = self.client.get("/lists/the-only-list-in-the-world/")
+
+        self.assertContains(response, "事项1")
+        self.assertContains(response, "事项2")
--- lists/views.py
+++ lists/views.py
@@ -5,7 +5,11 @@ from lists.models import Item
 def home_page(request):
     if request.method == "POST":
         Item.objects.create(text=request.POST["item_text"])
-        return redirect("/")
+        return redirect("/lists/the-only-list-in-the-world/")
 
+    return render(request, "home.html")
+
+
+def view_list(request):
     items = Item.objects.all()
-    return render(request, "home.html", {"items": items})
+    return render(request, "list.html", {"items": items})
