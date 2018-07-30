d492002ae71b16a0b1ee804868ae91550eeecb64
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-07-30 09:00:06 +0800

    单元测试通过，新增url和视图add_item函数

--- lists/tests.py
+++ lists/tests.py
@@ -45,19 +45,25 @@ class ListViewTest(TestCase):
 
     def test_uses_list_template(self):
         ''' 测试清单视图使用了和首页不同的模版 '''
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        list_ = List.objects.create()
+        response = self.client.get(f"/lists/{list_.id}/")
         self.assertTemplateUsed(response, "list.html")
 
-    def test_display_all_items(self):
+    def test_display_only_items_for_that_list(self):
         ''' 测试应用首页可以显示创建过的待办事项 '''
-        list_ = List.objects.create()
-        Item.objects.create(text="事项1", list=list_)
-        Item.objects.create(text="事项2", list=list_)
+        correct_list = List.objects.create()
+        Item.objects.create(text="事项1", list=correct_list)
+        Item.objects.create(text="事项2", list=correct_list)
+        other_list = List.objects.create()
+        Item.objects.create(text="其他待办事项1", list=other_list)
+        Item.objects.create(text="其他待办事项2", list=other_list)
 
-        response = self.client.get("/lists/the-only-list-in-the-world/")
+        response = self.client.get(f"/lists/{correct_list.id}/")
 
         self.assertContains(response, "事项1")
         self.assertContains(response, "事项2")
+        self.assertNotContains(response, "其他待办事项1")
+        self.assertNotContains(response, "其他待办事项2")
 
 
 class NewListTest(TestCase):
@@ -70,9 +76,38 @@ class NewListTest(TestCase):
         self.assertEqual(new_item.text, "一个新的待办事项")
 
     def test_redirects_after_POST(self):
-        ''' 判断处理完POST请求后可以正确的重定向 '''
+        ''' 判断处理完POST请求后可以正确的重定向到指定新建清单的URL '''
         response = self.client.post("/lists/new",
                                     data={"item_text": "一个新的待办事项"})
-        self.assertEqual(response.status_code, 302)
-        self.assertEqual(response["location"],
-                         "/lists/the-only-list-in-the-world/")
+        new_list = List.objects.first()
+        self.assertRedirects(response, f"/lists/{new_list.id}/")
+
+
+class NewItemTest(TestCase):
+
+    def test_can_save_a_POST_request_to_an_existing_list(self):
+        ''' 测试能在已存在列表中正确的保存一个POST请求 '''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "已存在列表中的一个新的待办事项"}
+        )
+
+        self.assertEqual(Item.objects.count(), 1)
+        new_item = Item.objects.first()
+        self.assertEqual(new_item.text, "已存在列表中的一个新的待办事项")
+        self.assertEqual(new_item.list, correct_list)
+
+    def test_redirects_to_list_view(self):
+        ''' 测试已存在列表中保存一个POST请求后，页面能够正确的重定向 '''
+        other_list = List.objects.create()
+        correct_list = List.objects.create()
+
+        response = self.client.post(
+            f"/lists/{correct_list.id}/add_item",
+            data={"item_text": "已存在列表中的一个新的待办事项"}
+        )
+
+        self.assertRedirects(response, f"/lists/{correct_list.id}/")
--- lists/views.py
+++ lists/views.py
@@ -6,12 +6,19 @@ def home_page(request):
     return render(request, "home.html")
 
 
-def view_list(request):
-    items = Item.objects.all()
+def view_list(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    items = Item.objects.filter(list=list_)
     return render(request, "list.html", {"items": items})
 
 
 def new_list(request):
     list_ = List.objects.create()
     Item.objects.create(text=request.POST["item_text"], list=list_)
-    return redirect("/lists/the-only-list-in-the-world/")
+    return redirect(f"/lists/{list_.id}/")
+
+
+def add_item(request, list_id):
+    list_ = List.objects.get(id=list_id)
+    Item.objects.create(text=request.POST["item_text"], list=list_)
+    return redirect(f"/lists/{list_.id}/")
