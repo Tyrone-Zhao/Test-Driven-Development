330366a75bcdcdf6f9e2ea94869118b3442c6aa3
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-07-30 08:04:43 +0800

    修改数据库模型及单元测试，但是功能测试依旧失败

--- lists/tests.py
+++ lists/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -10,24 +10,35 @@ class HomePageTest(TestCase):
         self.assertTemplateUsed(response, "home.html")
 
 
-class ItemModelTest(TestCase):
+class ListAndItemModelsTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
         ''' 测试数据库可以保存和获取待办事项 '''
+        list_ = List()
+        list_.save()
+
         first_item = Item()
-        first_item.text = "第一个列表项"
+        first_item.text = "第一个待办事项"
+        first_item.list = list_
         first_item.save()
 
         second_item = Item()
-        second_item.text = "第二个列表项"
+        second_item.text = "第二个待办事项"
+        second_item.list = list_
         second_item.save()
+
+        saved_list = List.objects.first()
+        self.assertEqual(saved_list, list_)
 
         saved_items = Item.objects.all()
         self.assertEqual(saved_items.count(), 2)
+
         first_saved_item = saved_items[0]
         second_saved_item = saved_items[1]
-        self.assertEqual(first_saved_item.text, "第一个列表项")
-        self.assertEqual(second_saved_item.text, "第二个列表项")
+        self.assertEqual(first_saved_item.text, "第一个待办事项")
+        self.assertEqual(first_saved_item.list, list_)
+        self.assertEqual(second_saved_item.text, "第二个待办事项")
+        self.assertEqual(second_saved_item.list, list_)
 
 
 class ListViewTest(TestCase):
@@ -39,8 +50,9 @@ class ListViewTest(TestCase):
 
     def test_display_all_items(self):
         ''' 测试应用首页可以显示创建过的待办事项 '''
-        Item.objects.create(text="事项1")
-        Item.objects.create(text="事项2")
+        list_ = List.objects.create()
+        Item.objects.create(text="事项1", list=list_)
+        Item.objects.create(text="事项2", list=list_)
 
         response = self.client.get("/lists/the-only-list-in-the-world/")
 
--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-from lists.models import Item
+from lists.models import Item, List
 
 
 def home_page(request):
@@ -12,5 +12,6 @@ def view_list(request):
 
 
 def new_list(request):
-    Item.objects.create(text=request.POST["item_text"])
+    list_ = List.objects.create()
+    Item.objects.create(text=request.POST["item_text"], list=list_)
     return redirect("/lists/the-only-list-in-the-world/")
