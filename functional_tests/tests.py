0aa24b3107dcc588c0ff9c683f175f86d8d12210
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-02 21:00:01 +0800

    开始添加布局和样式的功能测试reset --hard

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -117,7 +117,18 @@ class NewVisitorTest(LiveServerTestCase):
         self.browser.get(self.live_server_url)
         self.browser.set_window_size(1024, 768)
 
-        # 她看到输入框完美地居中显示
+        # 他看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10
+        )
+
+        # 他新建了一个清单，看到输入框仍完美地居中显示
+        inputbox.send_keys("测试")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 测试")
         inputbox = self.browser.find_element_by_id("id_new_item")
         self.assertAlmostEqual(
             inputbox.location["x"] + inputbox.size["width"] / 2,
