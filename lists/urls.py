363c71b8eb911dff5d06b221df63adeb4195037f
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-02 22:42:02 +0800

    用bootstrap改善布局

--- lists/urls.py
+++ lists/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import url
 from lists import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     url(r"^new$", views.new_list, name="new_list"),
     url(r"^(\d+)/$", views.view_list, name="view_list"),
     url(r"^(\d+)/add_item$", views.add_item, name="add_item"),
 ]
+urlpatterns += staticfiles_urlpatterns()
