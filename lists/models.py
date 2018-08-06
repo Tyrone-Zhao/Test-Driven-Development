8675a3b77fbf7084032cb2e34259982a933b7394
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 21:36:51 +0800

    所有单元测试和功能测试通过

--- lists/models.py
+++ lists/models.py
@@ -13,7 +13,7 @@ class Item(models.Model):
     list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
 
     class Meta:
-        # ordering = ("id",)
+        ordering = ("id",)
         unique_together = ("list", "text")
 
     def __str__(self):
