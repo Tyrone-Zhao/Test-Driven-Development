4168bfeac8c8c5c74cef11383ce70a402e46623a
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 18:01:55 +0800

    在所有视图函数中使用表单，单元测试和功能测试全部通过

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -11,27 +11,34 @@ class ItemValidationTest(FunctionalTest):
         self.browser.get(self.live_server_url)
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # 首页刷新了，显示一个错误消息
-        # 提示待办事项不能为空
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(".has-error").text,
-            "You can't have an empty list item"))
+        # 浏览器截获了请求
+        # 清单页面不会加载
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            "#id_text:invalid"))
 
-        # 他输入一些文字，然后再次提交，这次没问题了
+        # 他在待办事项中输入了一些文字
+        # 错误消失了
         self.get_item_input_box().send_keys("买牛奶")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            "#id_text:valid"
+        ))
+
+        # 现在能提交了
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: 买牛奶")
 
-        # 他有点调皮，又提交了一个空待办事项
+        # 他打算再提交一个空待办事项
         self.get_item_input_box().send_keys(Keys.ENTER)
 
-        # 在清单页面他看到了一个类似的错误消息
-        self.wait_for(lambda: self.assertEqual(
-            self.browser.find_element_by_css_selector(".has-error").text,
-            "You can't have an empty list item"))
+        # 浏览器这次也不会放行
+        self.wait_for_row_in_list_table("1: 买牛奶")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            "#id_text:invalid"))
 
-        # 输入文字之后就没问题了
+        # 输入一些文字后就能纠正这个错误
         self.get_item_input_box().send_keys("做壶茶")
+        self.wait_for(lambda: self.browser.find_element_by_css_selector(
+            "#id_text:valid"))
         self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table("1: 买牛奶")
         self.wait_for_row_in_list_table("2: 做壶茶")
--- lists/views.py
+++ lists/views.py
@@ -11,28 +11,21 @@ def home_page(request):
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    error = None
-
+    form = ItemForm()
     if request.method == "POST":
-        try:
-            item = Item(text=request.POST["text"], list=list_)
-            item.full_clean()
-            item.save()
+        form = ItemForm(data=request.POST)
+        if form.is_valid():
+            Item.objects.create(text=request.POST["text"], list=list_)
             return redirect(list_)
-        except ValidationError:
-            error = "You can't have an empty list item"
 
-    return render(request, "list.html", {"list": list_, "error": error})
+    return render(request, "list.html", {"list": list_, "form": form})
 
 
 def new_list(request):
-    list_ = List.objects.create()
-    item = Item(text=request.POST["text"], list=list_)
-    try:
-        item.full_clean()
-        item.save()
-    except ValidationError:
-        list_.delete()
-        error = "You can't have an empty list item"
-        return render(request, "home.html", {"error": error})
-    return redirect(list_)
+    form = ItemForm(data=request.POST)
+    if form.is_valid():
+        list_ = List.objects.create()
+        Item.objects.create(text=request.POST["text"], list=list_)
+        return redirect(list_)
+    else:
+        return render(request, "home.html", {"form": form})
