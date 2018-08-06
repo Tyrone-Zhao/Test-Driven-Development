d7f663d49639c9424bede38553c8e47f2756bf2a
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 13:36:20 +0800

    让列表视图判断数据库存储是否生效

--- lists/views.py
+++ lists/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from lists.models import Item, List
+from django.core.exceptions import ValidationError
 
 
 def home_page(request):
@@ -13,7 +14,14 @@ def view_list(request, list_id):
 
 def new_list(request):
     list_ = List.objects.create()
-    Item.objects.create(text=request.POST["item_text"], list=list_)
+    item = Item(text=request.POST["item_text"], list=list_)
+    try:
+        item.full_clean()
+        item.save()
+    except ValidationError:
+        list_.delete()
+        error = "You can't have an empty list item"
+        return render(request, "home.html", {"error": error})
     return redirect(f"/lists/{list_.id}/")
 
 
