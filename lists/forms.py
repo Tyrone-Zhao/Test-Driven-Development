fb0f30047c77deb9d9f69c7904fd7db85cb1a49c
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 15:59:16 +0800

    为待办事项列表创建新表单

---
+++ lists/forms.py
@@ -0,0 +1,22 @@
+from django import forms
+
+from lists.models import Item
+
+
+EMPTY_ITEM_ERROR = "You can't have an empty list item"
+
+
+class ItemForm(forms.models.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ("text",)
+        widgets = {
+            'text': forms.fields.TextInput(attrs={
+                "placeholder": "输入一个待办事项",
+                'class': 'form-control input-lg',
+            }),
+        }
+        error_messages = {
+            "text": {"required": EMPTY_ITEM_ERROR}
+        }
---
+++ lists/tests/test_forms.py
@@ -0,0 +1,26 @@
+from django.test import TestCase
+
+from lists.forms import ItemForm, EMPTY_ITEM_ERROR
+
+
+class ItemFormTest(TestCase):
+
+    def test_form_renders_item_text_input(self):
+        ''' 显示表单输入框的渲染内容 '''
+        form = ItemForm()
+        self.fail(form.as_p())
+
+    def test_form_item_input_has_placeholder_and_css_classes(self):
+        ''' 测试表单输入框的属性包含placeholder和class '''
+        form = ItemForm()
+        self.assertIn('placeholder="输入一个待办事项"', form.as_p())
+        self.assertIn('class="form-control input-lg"', form.as_p())
+
+    def test_form_validation_for_blank_items(self):
+        ''' 测试表单对空元素的校验 '''
+        form = ItemForm(data={"text": ""})
+        self.assertFalse(form.is_valid())
+        self.assertEqual(
+            form.errors["text"],
+            [EMPTY_ITEM_ERROR]
+        )
