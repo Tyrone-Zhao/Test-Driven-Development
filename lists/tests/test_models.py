b503bfb1306a83f9a9eda1c7f63bc436eea32bd7
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 14:51:50 +0800

    在List模型上用get_absolute_url解决视图url硬编码

--- lists/tests/test_models.py
+++ lists/tests/test_models.py
@@ -40,3 +40,8 @@ class ListAndItemModelsTest(TestCase):
         with self.assertRaises(ValidationError):
             item.save()
             item.full_clean()
+
+    def test_get_absolute_url(self):
+        ''' 测试Django的模型对象URL '''
+        list_ = List.objects.create()
+        self.assertEqual(list_.get_absolute_url(), f"/lists/{list_.id}/")
--- lists/views.py
+++ lists/views.py
@@ -16,7 +16,7 @@ def view_list(request, list_id):
             item = Item(text=request.POST["item_text"], list=list_)
             item.full_clean()
             item.save()
-            return redirect(f"/lists/{list_.id}/")
+            return redirect(list_)
         except ValidationError:
             error = "You can't have an empty list item"
 
@@ -33,4 +33,4 @@ def new_list(request):
         list_.delete()
         error = "You can't have an empty list item"
         return render(request, "home.html", {"error": error})
-    return redirect(f"/lists/{list_.id}/")
+    return redirect(list_)
