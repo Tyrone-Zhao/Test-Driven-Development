231a3c6c3506cdc5bb7a2c8dfc8518d35a04182d
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-06 10:36:01 +0800

    把功能测试移动到他们自己独立的文件

---
+++ functional_tests/test_layout_and_styling.py
@@ -0,0 +1,29 @@
+from selenium.webdriver.common.keys import Keys
+from .base import FunctionalTest
+
+
+class LayoutAndStylingTest(FunctionalTest):
+
+    def test_layout_and_styling(self):
+        # 小明访问首页
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 他看到输入框完美地居中显示
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10
+        )
+
+        # 他新建了一个清单，看到输入框仍完美地居中显示
+        inputbox.send_keys("测试")
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table("1: 测试")
+        inputbox = self.browser.find_element_by_id("id_new_item")
+        self.assertAlmostEqual(
+            inputbox.location["x"] + inputbox.size["width"] / 2,
+            512,
+            delta=10
+        )
