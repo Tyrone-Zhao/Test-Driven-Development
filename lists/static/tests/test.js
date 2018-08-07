10b9bef8eec454b1118598f934a2c4b7421210fa
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-07 15:22:30 +0800

    用onload样板代码确保脚本执行前加载了所有的Dom元素，为initialize函数增加命名空间

--- lists/static/tests/test.js
+++ lists/static/tests/test.js
@@ -1,10 +1,10 @@
 QUnit.test( "errors should be hidden on keypress", function (assert) {
-    initialize();
+    window.Superlists.initialize();
     $("input[name='text']").trigger("keypress");
     assert.equal($(".has-error").is(":visible"), false);
 });
 
 QUnit.test("errors aren't hidden if there is no keypress", function (assert) {
-    initialize();
+    window.Superlists.initialize();
     assert.equal($(".has-error").is(":visible"), true);
-})
+});
