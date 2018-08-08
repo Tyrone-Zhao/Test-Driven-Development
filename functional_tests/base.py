9a15149c9cf406833af0e353eb31371123e9f846
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-08 21:05:31 +0800

    重构base.py，增加装饰器，测试全部通过

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -8,6 +8,20 @@ import os
 MAX_WAIT = 10
 
 
+def wait(fn):
+    ''' 装饰器，不断调用指定的函数，并捕获常规的异常，直到超时为止 '''
+    def modified_fn(*args, **kwargs):
+        start_time = time.time()
+        while True:
+            try:
+                return fn(*args, **kwargs)
+            except (AssertionError, WebDriverException) as e:
+                if time.time() - start_time > MAX_WAIT:
+                    raise e
+                time.sleep(0.5)
+    return modified_fn
+
+
 class FunctionalTest(StaticLiveServerTestCase):
 
     def setUp(self):
@@ -20,47 +34,31 @@ class FunctionalTest(StaticLiveServerTestCase):
         time.sleep(10)
         self.browser.quit()
 
-    def wait_for_row_in_list_table(self, row_text):
-        ''' 重构后的辅助方法 '''
-        start_time = time.time()
-        while True:
-            try:
-                table = self.browser.find_element_by_id("id_list_table")
-                rows = table.find_elements_by_tag_name("tr")
-                self.assertIn(row_text, [row.text for row in rows])
-                return
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
-    def wait_for(self, fn):
-        ''' 函数式编程的等待方法 '''
-        start_time = time.time()
-        while True:
-            try:
-                return fn()
-            except (AssertionError, WebDriverException) as e:
-                if time.time() - start_time > MAX_WAIT:
-                    raise e
-                time.sleep(0.5)
-
     def get_item_input_box(self):
         ''' 重构功能测试中需要定位id_new_item的输入框 '''
         return self.browser.find_element_by_id("id_text")
 
+    @wait
+    def wait_for(self, fn):
+        return fn()
+
+    @wait
+    def wait_for_row_in_list_table(self, row_text):
+        ''' 等待待办事项输入框出现在页面中 '''
+        table = self.browser.find_element_by_id("id_list_table")
+        rows = table.find_elements_by_tag_name("tr")
+        self.assertIn(row_text, [row.text for row in rows])
+
+    @wait
     def wait_to_be_logged_in(self, email):
         ''' 等待登录成功 '''
-        self.wait_for(
-            lambda: self.browser.find_element_by_link_text("注销")
-        )
+        self.browser.find_element_by_link_text("注销")
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertIn(email, navbar.text)
 
+    @wait
     def wait_to_be_logged_out(self, email):
         ''' 等待注销成功 '''
-        self.wait_for(
-            lambda: self.browser.find_element_by_name("email")
-        )
+        self.browser.find_element_by_name("email")
         navbar = self.browser.find_element_by_css_selector(".navbar")
         self.assertNotIn(email, navbar.text)
