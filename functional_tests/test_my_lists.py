d96c5d2b09e5433aaf18a459db7d57db4deea833
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-09 10:27:46 +0800

    修改功能测试test_my_lists,解决过渡服务器功能测试不通过的问题

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -1,28 +1,25 @@
 from django.conf import settings
-from django.contrib.auth import BACKEND_SESSION_KEY, SESSION_KEY, get_user_model
-from django.contrib.sessions.backends.db import SessionStore
-from .base import FunctionalTest
 
-User = get_user_model()
+from .base import FunctionalTest
+from .server_tools import create_session_on_server
+from .management.commands.create_session import create_pre_authenticated_session
 
 
 class MyListTest(FunctionalTest):
 
     def create_pre_authenticated_session(self, email):
         ''' 创建之前的认证会话 '''
-
-        user = User.objects.create(email=email)
-        session = SessionStore()
-        session[SESSION_KEY] = user.pk
-        session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-        session.save()
+        if self.staging_server:
+            session_key = create_session_on_server(self.staging_server, email)
+        else:
+            session_key = create_pre_authenticated_session(email)
 
         ## 为了设定cookie, 我们要先访问网站
         # 而404页面是加载最快的
         self.browser.get(self.live_server_url + "/404_no_such_url/")
         self.browser.add_cookie(dict(
             name=settings.SESSION_COOKIE_NAME,
-            value=session.session_key,
+            value=session_key,
             path="/",
         ))
 
