aac9ee8acbf95b2f3dfce062a4d59ec7eee22aac
Tyrone-Zhao <tyrone-zhao@qq.com>
2018-08-09 10:27:46 +0800

    修改功能测试test_my_lists,解决过渡服务器功能测试不通过的问题

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -4,6 +4,8 @@ from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 import time
 import os
 
+from .server_tools import reset_database
+
 
 MAX_WAIT = 10
 
@@ -29,6 +31,7 @@ class FunctionalTest(StaticLiveServerTestCase):
         self.staging_server = os.environ.get("STAGING_SERVER")
         if self.staging_server:
             self.live_server_url = "http://" + self.staging_server
+            reset_database(self.staging_server)
 
     def tearDown(self):
         time.sleep(10)
